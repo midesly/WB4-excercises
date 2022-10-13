850cc7c774c45ab6abbfcb269e13ee015bfc80ac
midesly <ayofrancesakin@gmail.com>
2022-10-13 05:30:49 -0400

    loop practice complete

---
+++ loopScripts.js
@@ -0,0 +1,9 @@
+for(let i = 0; i < 5; i++){
+    console.log(i, "I LOVE LOOPS");
+}
+
+let i = 0;
+while(i < 5){
+    console.log(i, "I LOVE LOOPS");
+    i++
+}
---
+++ loop_example.js
@@ -0,0 +1,25 @@
+// while loop example
+
+let num = 1;
+let i = 1;
+
+while (i < 5){
+    num = num * 2;
+    console.log(num);
+    i++;
+}
+
+// do/while loop examples
+
+ do {
+    num = num * 2;
+    console.log(num);
+    i++;
+ } while(i < 5);
+
+ // for loop example
+
+ for(i = 0; i < 5; i++){
+    num = num * 2;
+    console.log(num);
+ }
